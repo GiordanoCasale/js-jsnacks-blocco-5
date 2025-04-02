a98e85e3c446b3fe44a4a410f02b7dcf9303e10a
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-02 14:24:38 +0200

    [UPD] snack 1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,4 +3,4 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 //andiamo ad usare il console.log per stampare i nomi nella console
-console.log()
+console.log(names)
